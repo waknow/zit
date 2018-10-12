e0de1cfe76af3f92539a61ffe442b5280bc3298d
Mengqiang Zhu <zhumq@next-stage.com.cn>
2018-10-12 17:03:54 +0800

    change some

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # zit
+
+that is...
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"compress/zlib"
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	const content = "what is up, doc?"
+
+	filepath.Walk(`.git\objects`, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		t, c, err := readObject(path)
+		if err != nil {
+			log.Println("Read Object Failed", err)
+		} else {
+			log.Printf("%s => type: %s, content: '%s'", path, t, sPrint(t, c))
+		}
+		return nil
+	})
+
+	// t, c, err := readObject(`.git\objects\b5\5c4b24b098a2b4890f120b8aae44f5f8e59472`)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// } else {
+	// 	log.Printf("type: %s, content: %s", t, c)
+	// }
+}
+
+func sPrint(objType string, content []byte) string {
+	switch objType {
+	case "blob":
+		return string(content)
+	case "tree":
+		index := bytes.Index(content, []byte("\000"))
+		return string(content[:index]) + " " + fmt.Sprintf("%x", content[index+1:])
+	case "commit":
+		return string(content)
+	default:
+		return ""
+	}
+}
+
+func readObject(filename string) (string, []byte, error) {
+	f, err := os.OpenFile(filename, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return "", nil, err
+	}
+	defer f.Close()
+
+	reader, err := zlib.NewReader(f)
+	if err != nil {
+		return "", nil, err
+	}
+
+	bs, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", nil, err
+	}
+
+	index := bytes.Index(bs, []byte("\000"))
+	parts := bytes.Split(bs[:index], []byte(" "))
+	t := string(parts[0])
+	return t, bs[index+1:], nil
+}
+
+func writeObject(content string) {
+	header := fmt.Sprintf("blob %d\000", len(content))
+	log.Println("header:", header)
+
+	store := header + content
+	id := fmt.Sprintf("%x", sha1.Sum([]byte(store)))
+	log.Println("id", id)
+
+	buf := new(bytes.Buffer)
+	writer := zlib.NewWriter(buf)
+	_, err := writer.Write([]byte(store))
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.Flush()
+	writer.Close()
+
+	path := ".zit/objects/" + id[0:2] + "/"
+	log.Println("filepath", path)
+
+	os.MkdirAll(path, os.ModePerm)
+	ioutil.WriteFile(path+id[2:], buf.Bytes(), os.ModePerm)
+}
